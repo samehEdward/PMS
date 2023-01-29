44378d951074a81331c8432743635372475c2267
Sameh Edward <edw21629@spengergasse.at>
2023-01-29 11:39:07 +0100

    User Form

--- src/main/java/at/spengegasse/pms/web/HelloController.java
+++ src/main/java/at/spengegasse/pms/web/HelloController.java
@@ -8,8 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class HelloController
 {
-    @RequestMapping(value = "/basic", method = RequestMethod.GET)
+    @RequestMapping("/basic")
     public String sayHello(){
         return "<h1>Hello</h1>";
     }
+
+    @RequestMapping("/user_entry")
+    public String userForm()
+    {
+  return   "<form action=\"/user_greeting\" method=\"POST\">\n" +
+            "  <label for=\"fname\">First name:</label><br>\n" +
+            "  <input type=\"text\" id=\"fname\" name=\"fname\" value=\"John\"><br>\n" +
+            "  <label for=\"lname\">Last name:</label><br>\n" +
+            "  <input type=\"text\" id=\"lname\" name=\"lname\" value=\"Doe\"><br><br>\n" +
+            "  <input type=\"submit\" value=\"Submit\">\n" +
+            "</form> ";
+    }
+
+
 }
