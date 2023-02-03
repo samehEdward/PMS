4b6f2a958823fdb3eecab928bb63bdaa2d523a11
Sameh Edward <edw21629@spengergasse.at>
2023-02-03 16:30:38 +0100

    handel saving to the Database

--- src/main/java/at/spengegasse/pms/controllers/ProjectContoller.java
+++ src/main/java/at/spengegasse/pms/controllers/ProjectContoller.java
@@ -4,13 +4,15 @@ import at.spengegasse.pms.entities.Project;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectContoller
 {
-    @RequestMapping("/new")
+    @GetMapping("/new")
    public String displayProjectForm(Model model){
 
         Project proj = new Project();
@@ -20,8 +22,9 @@ public class ProjectContoller
         return "new-projects";
    }
 
-   @RequestMapping("/save")
-    public String createProject(){
+   @PostMapping("/save")
+    public String createProject(Project project, Model model){
+    // this should handel saving to the Database
         return null;
     }
 
