d7906f92c787f296ac0e418911e641fd4ebe3f0c
Sameh Edward <edw21629@spengergasse.at>
2023-02-03 16:11:43 +0100

    CRUD First Step

--- src/main/java/at/spengegasse/pms/controllers/ProjectContoller.java
+++ src/main/java/at/spengegasse/pms/controllers/ProjectContoller.java
@@ -1,6 +1,9 @@
 package at.spengegasse.pms.controllers;
 
+import at.spengegasse.pms.entities.Project;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectContoller
 {
     @RequestMapping("/new")
-   public String displayProjectForm(){
+   public String displayProjectForm(Model model){
+
+        Project proj = new Project();
+        model.addAttribute("project", proj);
+
+
         return "new-projects";
    }
 }
