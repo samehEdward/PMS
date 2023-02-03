eb38014c85eb83c159de597e9fb57a314813da45
Sameh Edward <edw21629@spengergasse.at>
2023-02-03 14:52:11 +0100

    Project Class and Project controller created

---
+++ src/main/java/at/spengegasse/pms/controllers/ProjectContoller.java
@@ -0,0 +1,14 @@
+package at.spengegasse.pms.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectContoller
+{
+    @RequestMapping("/new")
+   public String displayProjectForm(){
+        return "new-projects";
+   }
+}
