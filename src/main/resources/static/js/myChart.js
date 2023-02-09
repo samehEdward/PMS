92f54f9a69ff417abef1f5a44c3e708abd5848f9
Sameh Edward <edw21629@spengergasse.at>
2023-02-09 18:30:51 +0100

    Chart configration

---
+++ src/main/resources/static/js/myChart.js
@@ -0,0 +1,14 @@
+
+new Chart(document.getElementById("myPieChart"), {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+
+    options:{}
+});
